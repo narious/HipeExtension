724d7b246308463b065ee7faa4d07a9a8b75490a
michael d <tonkoalsce@gmail.com>
2021-07-30 20:45:18 +1000

    added files to define structure

---
+++ HtmlToHipe/src/htmltohipe.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int add(int x, int y) 
+{
+    int res = x + y;
+    std::cout << res << std::endl;
+    return res;
+}
---
+++ HtmlToHipe/src/test.cpp
@@ -0,0 +1,6 @@
+#include "gtest/gtest.h"
+
+TEST(t1, sample1)
+{
+    EXPECT_EQ(1, 1);
+}

9b7a2adbc0e99a6fec13444e78c2dc9dbd88d381
Michael dinh <mdin16@studdent.monash.edu>
2021-07-31 14:59:36 +1000

    gtest for hipe extnsion work tofix: linkotherfiles

--- HtmlCsstoHipe.cpp
+++ HtmlCsstoHipe.cpp
@@ -2,13 +2,9 @@
 #include "htmltohipe.h"
 #include "gtest/gtest.h"
 
-int main() 
-{
-    std::cout << "Hello World" << std::endl;
-    return 0;
-}
-
 TEST(t1, t1)
 {
+    std::cout << add(1, 1) << std::endl;
     EXPECT_EQ(1, 1);
+    
 }

f6d8ccc856ac57995785b207ec92826cfb66a111
pt <petarturukalo@gmail.com>
2021-08-17 13:22:38 +1000

    create phase 1 & 2 files

---
+++ HtmlToHipe/src/attr.h
@@ -0,0 +1,8 @@
+/*
+ * Set all HTML tag attributes in a HTML file with hipe
+ * client instructions.
+ */
+#ifndef ATTR_H
+#define ATTR_H
+
+#endif
---
+++ HtmlToHipe/src/tag.h
@@ -0,0 +1,8 @@
+/*
+ * Create all HTML tags in a HTML file with hipe
+ * client instructions.
+ */
+#ifndef TAG_H
+#define TAG_H
+
+#endif

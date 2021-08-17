64ca0a5a5e6482cb5696adc49ab8277b9161d346
Michael dinh <mdin16@studdent.monash.edu>
2021-08-17 13:32:58 +1000

    writing a traverse function

--- HtmlToHipe/src/HtmlToHipe.h
+++ HtmlToHipe/src/HtmlToHipe.h
@@ -5,6 +5,11 @@
 static void traverse_and_print(GumboNode* node, std::ostream& outputfile)
 {
     if (node->type != GUMBO_NODE_ELEMENT) return;
+    printf("%s", node->v.text);
+    GumboVector* children = &node->v.element.children;
+    for (unsigned int i = 0; i < children->length; i ++) {
+        (GumboNode*)children[i]->v.text;
+    }
 
 }
 
